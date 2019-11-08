3c5baca91b28f8a22bb84186e8dd3dc88044a201
GuyFromHere <47902660+GuyFromHere@users.noreply.github.com>
2019-11-07 17:02:57 -0800

    Adding more pieces...testing element interaction...

--- assets/scripts/javascript.js
+++ assets/scripts/javascript.js
@@ -8,36 +8,65 @@
 // set manual timer variable for testing
 var countDown = 5;
 
+// DOM Object Style Guide:
+// https://www.w3schools.com/jsref/dom_obj_style.asp
 /* <h1>Coding Quiz Challenge</h1>
 <p>Click the Start Quiz button to start the quiz!</p>
 <input type="button" value="Start Quiz" id="startButton" onclick="quiz();" /> */
 
+function setProperties(element, props) {
+  for (var key in props) {
+    element.style.key = props[key];
+    console.log(key + " " + props[key]);
+  }
+}
+
 function buildGame() {
   // load quiz in to DOM and collapse / hide elements until they are called...
-  var newNode = document.createElement("div");
-  newNode.className = "gameBoard";
+
+  var board = document.getElementById("gameBoard");
+  board.style.textAlign = "center";
+
+  var newNode = document.createElement("h1");
+  newNode.innerHTML = "Coding Quiz Challenge";
+  board.appendChild(newNode);
+
+  newNode = document.createElement("p");
+  newNode.innerHTML = "Click the Start Quiz button to start the quiz!";
+  board.appendChild(newNode);
+
+  newNode = document.createElement("input");
+  newNode.setAttribute("type", "button");
+  newNode.setAttribute("value", "Start Quiz");
+  newNode.setAttribute("id", "startButton");
+  newNode.setAttribute("onclick", "quiz();");
+  board.appendChild(newNode);
 }
 
 function gameOver() {
   // When game ends, display message and show button to restart the quiz (test)
-  // Get first child of quizBoard
-  var board = document.getElementById("quizBoard");
-
+  // Clear children of board
+  var board = document.getElementById("gameBoard");
+  while (board.firstChild) {
+    board.firstChild.remove();
+  }
   // Create new child nodes
-  var result = document.createTextNode("");
+  var newNode = document.createElement("div");
+  newNode.innerHTML = "New Node!";
+  board.appendChild(newNode);
 }
 
 function quiz() {
   // set sec to test value
   var sec = countDown;
 
-  buildQuiz();
   // start timer
   time = setInterval(function() {
     if (sec > 0) {
       document.getElementById("timer").innerHTML = sec--;
     } else {
-      alert("GO");
+      /*alert("Game Over");*/
+      gameOver();
       clearInterval(time);
       sec = countDown;
     }
