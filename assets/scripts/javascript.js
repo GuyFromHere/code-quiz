e7dddfeecc91633c4502161306f942c7239e37c2
Dan Dismuke <dandismuke@gmail.com>
2019-11-09 13:18:37 -0800

    Styles on question results. Showing scores table and saving scores in object. TODO: store object in local storage to save score history.

--- assets/scripts/javascript.js
+++ assets/scripts/javascript.js
@@ -7,146 +7,183 @@
 // DOM Object Style Guide:
 // https://www.w3schools.com/jsref/dom_obj_style.asp
 
-/* var board = document.getElementById('gameBoard'); */
+/* window.board = document.getElementById('gameBoard'); */
 
 // Keep track of whether the game is still active.
 var game = true;
 var sec = questions.length * 10;
-var scores = {
-  players: [],
-  playerScores: []
+
+var scoresObj = {
+	players: new Array(),
+	playerScores: new Array()
 };
 
+//localStorage.setItem('scores', JSON.stringify(scores));
+
 function buildGame() {
-  // Called on body load. Creates the start page for the quiz.
-  var board = document.getElementById("gameBoard");
-  board.style.textAlign = "center";
+	// Called on body load. Creates the start page for the quiz.
+	var board = document.getElementById('gameBoard');
+	board.style.textAlign = 'center';
 
-  var newNode = document.createElement("h1");
-  newNode.innerHTML = "Coding Quiz Challenge";
-  board.appendChild(newNode);
+	var newNode = document.createElement('h1');
+	newNode.textContent = 'Coding Quiz Challenge';
+	board.appendChild(newNode);
 
-  newNode = document.createElement("p");
-  newNode.innerHTML = "Click the Start Quiz button to start the quiz!";
-  board.appendChild(newNode);
+	newNode = document.createElement('p');
+	newNode.textContent = 'Click the Start Quiz button to start the quiz!';
+	board.appendChild(newNode);
 
-  newNode = document.createElement("input");
-  newNode.setAttribute("type", "button");
-  newNode.setAttribute("value", "Start Quiz");
-  newNode.setAttribute("id", "startButton");
-  newNode.setAttribute("onclick", "quiz();");
-  board.appendChild(newNode);
+	newNode = document.createElement('input');
+	newNode.setAttribute('type', 'button');
+	newNode.setAttribute('value', 'Start Quiz');
+	newNode.setAttribute('id', 'startButton');
+	newNode.setAttribute('onclick', 'quiz();');
+	board.appendChild(newNode);
 }
 
 function getScore(correct) {
-  var messageArea = document.getElementById("messageArea");
-  if (correct) {
-    messageArea.className = "correct";
-    messageArea.textContent = "Correct!";
-  } else {
-    messageArea.className = "false";
-    messageArea.textContent = "False!";
-    sec -= 5;
-  }
+	var messageArea = document.getElementById('messageArea');
+	if (correct) {
+		messageArea.className = 'correct';
+		messageArea.textContent = 'Correct!';
+	} else {
+		messageArea.className = 'incorrect';
+		messageArea.textContent = 'Incorrect!';
+		sec -= 5;
+	}
 }
 
 function gameOver() {
-  // When game ends, display message and show button to restart the quiz (test)
-  game = false;
-  var board = document.getElementById("gameBoard");
-  while (board.firstChild) {
-    board.firstChild.remove();
-  }
+	// When game ends, display message and show button to restart the quiz (test)
+	game = false;
+	var board = document.getElementById('gameBoard');
+	// Prompt for initials
+	var playerName = prompt('Enter initials:');
 
-  // Create new child nodes
-  var newNode = document.createElement("div");
-  newNode.setAttribute("class", "gameOver");
-  newNode.innerHTML = "<h1>Game Over</h1><h4>High Scores:</h4>" + sec;
-  board.appendChild(newNode);
+	while (board.firstChild) {
+		board.firstChild.remove();
+	}
 
-  newNode = document.createElement("table");
-  newNode.setAttribute("class", "highScores");
-  board.lastChild.appendChild(newNode);
+	// Get user score and store it in scores object
+	scoresObj.players.push(playerName);
+	scoresObj.playerScores.push(sec.toString());
+
+	console.log(scoresObj.players);
+	console.log(scoresObj.playerScores);
+
+	// Create new child nodes
+	var newNode = document.createElement('div');
+	newNode.setAttribute('class', 'gameOver');
+	newNode.innerHTML = '<h1>Game Over</h1><h4>High Scores:</h4>';
+	board.appendChild(newNode);
+	// High Score table
+	newNode = document.createElement('table');
+	newNode.setAttribute('class', 'highScores');
+	board.lastChild.appendChild(newNode);
+	// th Name
+	newNode = document.createElement('th');
+	newNode.textContent = 'Name:';
+	board.lastChild.appendChild(newNode);
+	// th Score
+	newNode = document.createElement('th');
+	newNode.textContent = 'Score:';
+	board.lastChild.appendChild(newNode);
+	// Loop through scores...
+	for (var i = 0; i < scoresObj.players.length; i++) {
+		// tr
+		newNode = document.createElement('tr');
+		board.lastChild.appendChild(newNode);
+		// td
+		newNode = document.createElement('td');
+		newNode.textContent = scoresObj.players[i];
+		board.lastChild.appendChild(newNode);
+		// td
+		newNode = document.createElement('td');
+		newNode.textContent = scoresObj.playerScores[i];
+		board.lastChild.appendChild(newNode);
+	}
 }
 
 // Replaces current question with the specified item from the question array
 function nextQuestion(question, answer) {
-  // Clear gameBoard
-  var board = document.getElementById("gameBoard");
-  while (board.firstChild) {
-    board.firstChild.remove();
-  }
+	// Clear gameBoard
+	var board = document.getElementById('gameBoard');
+	while (board.firstChild) {
+		board.firstChild.remove();
+	}
 
-  var nextQuestion = question + 1;
-  var lastQuestion = question - 1;
+	var nextQuestion = question + 1;
+	var lastQuestion = question - 1;
 
-  if (nextQuestion > questions.length) {
-    gameOver();
-  } else {
-    // Create div to hold question
-    var newNode = document.createElement("div");
-    newNode.setAttribute("id", question);
-    board.appendChild(newNode);
+	if (nextQuestion > questions.length) {
+		game = false;
+	} else {
+		// Create div to hold question
+		var newNode = document.createElement('div');
+		newNode.setAttribute('id', question);
+		board.appendChild(newNode);
 
-    // Question
-    newNode = document.createElement("h1");
-    newNode.textContent = questions[question].title;
-    board.children[0].appendChild(newNode);
+		// Question
+		newNode = document.createElement('h1');
+		newNode.textContent = questions[question].title;
+		board.children[0].appendChild(newNode);
 
-    // Answers
-    newNode = document.createElement("ul");
-    newNode.setAttribute("id", "list_" + question);
-    board.lastChild.appendChild(newNode);
+		// Answers
+		newNode = document.createElement('ul');
+		newNode.setAttribute('id', 'list_' + question);
+		board.lastChild.appendChild(newNode);
 
-    var questionList = document.getElementById("list_" + question);
-    for (var x = 0; x < questions[question].choices.length; x++) {
-      questionList.insertAdjacentHTML(
-        "afterbegin",
-        "<li class='question' onclick='nextQuestion(" +
-          nextQuestion +
-          "," +
-          x +
-          ")')>" +
-          questions[question].choices[x] +
-          "</li>"
-      );
-    }
-    newNode = document.createElement("div");
-    newNode.setAttribute("id", "messageArea");
-    board.lastChild.appendChild(newNode);
+		var questionList = document.getElementById('list_' + question);
+		for (var x = 0; x < questions[question].choices.length; x++) {
+			questionList.insertAdjacentHTML(
+				'afterbegin',
+				"<li class='question' onclick='nextQuestion(" +
+					nextQuestion +
+					',' +
+					x +
+					")')>" +
+					questions[question].choices[x] +
+					'</li>'
+			);
+		}
+		newNode = document.createElement('div');
+		newNode.setAttribute('id', 'messageArea');
+		board.lastChild.appendChild(newNode);
 
-    // Score last question
-    if (answer > -1) {
-      if (
-        questions[lastQuestion].answer ===
-        questions[lastQuestion].choices[answer]
-      ) {
-        getScore(true);
-      } else if (
-        questions[lastQuestion].answer !=
-        questions[lastQuestion].choices[answer]
-      ) {
-        getScore(false);
-      }
-    }
-  }
+		// Score last question
+		if (answer > -1) {
+			if (
+				questions[lastQuestion].answer ===
+				questions[lastQuestion].choices[answer]
+			) {
+				getScore(true);
+			} else if (
+				questions[lastQuestion].answer !=
+				questions[lastQuestion].choices[answer]
+			) {
+				getScore(false);
+			}
+		}
+	}
 }
 
 function quiz() {
-  document.getElementById("timer").textContent = sec;
+	// Set timer
+	document.getElementById('timer').textContent = sec;
 
-  // Show first question
-  nextQuestion(0, -1);
+	// Show first question
+	nextQuestion(0, -1);
 
-  // start timer
-  time = setInterval(function() {
-    if (sec > 0 && game) {
-      sec--;
-      document.getElementById("timer").textContent = sec;
-    } else {
-      gameOver();
-      clearInterval(time);
-      sec = questions.length * 10;
-    }
-  }, 1000);
+	// Start timer
+	var time = setInterval(function() {
+		if (sec > 0 && game) {
+			sec--;
+			document.getElementById('timer').textContent = sec;
+		} else {
+			gameOver();
+			clearInterval(time);
+			/* return; */
+			/* sec = questions.length * 10; */
+		}
+	}, 1000);
 }
--- assets/scripts/questions.js
+++ assets/scripts/questions.js
@@ -1,12 +1,28 @@
 var questions = [
-  {
-    title: "Commonly used data types DO NOT include:",
-    choices: ["strings", "booleans", "alerts", "numbers"],
-    answer: "alerts"
-  },
-  {
-    title: "The condition in an if / else statement is enclosed within ____.",
-    choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
-    answer: "parentheses"
-  }
+	{
+		title: 'Commonly used data types DO NOT include:',
+		choices: ['strings', 'booleans', 'alerts', 'numbers'],
+		answer: 'alerts'
+	},
+	{
+		title:
+			'The condition in an if / else statement is enclosed within ____.',
+		choices: ['quotes', 'curly brackets', 'parentheses', 'square brackets'],
+		answer: 'parentheses'
+	},
+	{
+		title: 'Most of the time, dogs are born with ____ legs.',
+		choices: ['frog', 'four', 'frothy', 'Fortnite'],
+		answer: 'four'
+	},
+	{
+		title: 'Portland is widely known for:',
+		choices: [
+			'hot weather',
+			'lack of trees',
+			'rainy weather',
+			'tall buildings'
+		],
+		answer: 'rainy weather'
+	}
 ];
