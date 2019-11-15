43061b0a2b1f99566e102508625c94a8726fc2be
Dan Dismuke <dandismuke@gmail.com>
2019-11-14 20:59:22 -0800

    Added questions.

--- assets/scripts/questions.js
+++ assets/scripts/questions.js
@@ -1,28 +1,57 @@
 var questions = [
-	{
-		title: 'Commonly used data types DO NOT include:',
-		choices: ['strings', 'booleans', 'alerts', 'numbers'],
-		answer: 'alerts'
-	},
-	{
-		title:
-			'The condition in an if / else statement is enclosed within ____.',
-		choices: ['quotes', 'curly brackets', 'parentheses', 'square brackets'],
-		answer: 'parentheses'
-	},
-	{
-		title: 'Most of the time, dogs are born with ____ legs.',
-		choices: ['frog', 'four', 'frothy', 'Fortnite'],
-		answer: 'four'
-	},
-	{
-		title: 'Portland is widely known for:',
-		choices: [
-			'hot weather',
-			'lack of trees',
-			'rainy weather',
-			'tall buildings'
-		],
-		answer: 'rainy weather'
-	}
+  {
+    title: "Commonly used data types DO NOT include:",
+    choices: ["strings", "booleans", "alerts", "numbers"],
+    answer: "alerts"
+  },
+  {
+    title: "The condition in an if / else statement is enclosed within ____.",
+    choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
+    answer: "parentheses"
+  },
+  {
+    title: "Which of the following is NOT a valid Javascript operator:",
+    choices: ["===", "&&", "||", "##"],
+    answer: "##"
+  },
+  {
+    title: "Which of the following is a valid Javascript keyword:",
+    choices: ["array", "zone", "note", "area"],
+    answer: "array"
+  },
+  {
+    title: "Inside which HTML element do we put the JavaScript?",
+    choices: ["<scripting>", "<javascript>", "<script>", "<js>"],
+    answer: "<script>"
+  },
+  {
+    title: "Where is the correct place to insert a JavaScript?",
+    choices: [
+      "The <head> section",
+      "The <body> section",
+      "The <section> section",
+      "The <main> section"
+    ],
+    answer: "The <head> section"
+  },
+  {
+    title:
+      'What is the correct syntax for referring to an external script called "xxx.js"?',
+    choices: [
+      "<script href='xxx.js'>",
+      "<script src='xxx.js'>",
+      "<script location='xxx.js'>",
+      "<script source='xxx.js'>"
+    ],
+    answer: "<script src='xxx.js'>"
+  },
+  {
+    title: "What does HTML stand for?",
+    choices: [
+      "Home Tool Markup Language",
+      "Hyper Text and Links Markup Language",
+      "Hyper Text Markup Language",
+      "Howsabout This Markup Language"
+    ]
+  }
 ];
